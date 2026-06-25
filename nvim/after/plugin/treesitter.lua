0bc244fd00ae6153156158135e3b886cf0b3a62d
Dimitar Ralev <dimitar@rallyo.io>
2026-06-25 11:15:18 +0300

    update nvim

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = "all",
+    ensure_installed = { "go", "typescript", "javascript", "tsx", "lua", "bash", "json", "yaml", "markdown" },
     sync_install = false,
+    prefer_git = true,
 
     highlight = {
         enable = true,
